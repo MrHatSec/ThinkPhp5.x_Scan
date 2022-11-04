c37cdaff45ae5893d70fd4aa1789955e238c0db2
MrHatSec <71647931+MrHatSec@users.noreply.github.com>
2022-11-04 17:54:24 +0800

    Add files via upload

---
+++ utils/removeRepeatedElement.go
@@ -0,0 +1,18 @@
+package utils
+
+func RemoveRepeatedElement(arr []string) []string {
+	newArr := make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return newArr
+}
